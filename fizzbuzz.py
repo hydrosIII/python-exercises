2e0d8d04381003ae9d517800fc32842db41c7c0c
hydrosIII <galprasmarco@gmail.com>
2015-04-01 00:53:43 -0600

    more efficient ways to solve fizzbuz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,26 +1,39 @@
 import math
 
 
-
-def fizzbuz ():
-
-
-        for i in range (1,100):
-
-
-
-            o = i/3.0
-            j = i/5.0
-            f = i/15.0
-
-            if (o).is_integer() is True:
-                i = "buz"
-            if (j).is_integer() is True:
-                i= "fiz"
-            if (f).is_integer() is True:
-                i= "fizbuz"
-
-            print i
+def fizzbuzz_modulo():
+    for i in range(0, 100):
+        #r = i
+        if i % 15 == 0:
+            i = "fizzbuzz"
+        elif i % 3 == 0:
+            i = "fizz"
+        elif i % 5 == 0:
+            i = "buzz"
+        
+        print i
 
 
+def fizzbuz():
+    for i in range(1, 100):
+        o = i / 3.0
+        j = i / 5.0
+        f = i / 15.0
+           
+
+        if (f).is_integer() is True:
+            i = "fizbuz"
+
+        elif (o).is_integer() is True:
+            i = "buz"
+
+        elif (j).is_integer() is True:
+            i = "fiz"
+
+        
+
+        print i
+
+
+#fizzbuzz_modulo()
 fizzbuz()
